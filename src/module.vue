de9bd3cc91e2fd9b3d10dd613506f04cb24d97c6
kev2019 <45583648+kev2019@users.noreply.github.com>
2020-12-19 23:16:55 +1100

    Initial Commit

---
+++ src/module.vue
@@ -0,0 +1,93 @@
+<template>
+  <private-view :title="$tc('Analytics', 2)">
+    <template #title-outer:prepend>
+      <v-button class="header-icon" rounded disabled icon secondary>
+        <v-icon name="insert_chart_outlined" />
+      </v-button>
+    </template>
+    <h1>{{ title }}</h1>
+    <template>
+      <v-input v-model="title">
+        <template #prepend><v-icon name="person_search"/></template>
+      </v-input>
+      <v-select v-model="dropdownValue" :items="dropdownOptions">
+        <template #prepend><v-icon name="person_search"/></template>
+      </v-select>
+    </template>
+    <template #sidebar>
+      <sidebar-detail icon="info_outline" :title="$t('information')" close>
+        <div
+          class="page-description"
+          v-html="$t('page_help_collections_item')"
+        />
+      </sidebar-detail>
+      <!-- <revisions-drawer-detail
+				v-if="isNew === false && _primaryKey"
+				:collection="collection"
+				:primary-key="_primaryKey"
+				ref="revisionsDrawerDetail"
+				@revert="refresh"
+			/>
+			<comments-sidebar-detail
+				v-if="isNew === false && _primaryKey"
+				:collection="collection"
+				:primary-key="_primaryKey"
+			/> -->
+    </template>
+  </private-view>
+</template>
+
+<script>
+import { computed } from "@vue/composition-api";
+
+export default {
+  inject: ["system"],
+  mounted() {
+    console.log("option api", this.system);
+
+    // console.log(this.system.useCollectionsStore().getCollection("patients"));
+    // console.log(this.getCollections());
+
+    this.dropdownOptions = this.getCollections();
+
+    console.log(this.system.useCollectionsStore().getCollection("patients"));
+    console.log(
+      this.system.useFieldsStore().getFieldsForCollection("patients")
+    );
+  },
+  data() {
+    return {
+      title: "New Title",
+      dropdownOptions: [],
+      dropdownValue: "",
+    };
+  },
+  methods: {
+    getCollections: function() {
+      return this.system
+        .useCollectionsStore()
+        .visibleCollections.value.map((collection) => {
+          return {
+            text: collection.name,
+            value: collection.collection,
+          };
+        });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.icon {
+  --v-icon-color: var(--foreground-subdued);
+
+  ::v-deep i {
+    vertical-align: unset;
+  }
+}
+
+.header-icon {
+  --v-button-color-disabled: var(--foreground-normal);
+}
+</style>
+
+import StyleComponent import Clock import Calendar Date
